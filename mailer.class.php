de89e51190ca4962000518aee664df0f9d982e07
sbeam <sbeam@onsetcorps.net>
2009-08-28 18:07:57 +0000

    refactor to improve lame mailer class to create its own headers

--- mailer.class.php
+++ mailer.class.php
@@ -6,30 +6,42 @@ class circusMailer {
 
     var $sendmail_path = '/usr/sbin/sendmail';
 
-    function send($recip, $subj, $msg, $enc="") {
+    var $email_sender = EMAIL_SENDER;
+    var $email_bcc = ERROR_EMAIL_RECIP;
+    var $email_return_to = EMAIL_SENDER;
+    var $encoding = null;
 
-        $headers['From']   = EMAIL_SENDER;
-        $headers['BCC']    = ERROR_EMAIL_RECIP; 
-        if ($enc) {
-            $headers['Content-type'] = 'text/plain; charset=' . $enc;
-            if (strtolower($enc) == 'utf-8') {
-                $subj = "=?UTF-8?B?" . base64_encode($subj) . "?=";
-            }
+    function get_headers() {
+        $headers = array();
+        $headers['From']   = $this->email_sender;
+        $headers["X-Sender"] = $this->email_sender; 
+        $headers['BCC']    = $this->email_bcc;
+        $headers["Return-Path"] = $this->email_return_to;  // Return path for errors
+
+        if ($this->encoding) {
+            $headers['Content-type'] = 'text/plain; charset=' . $this->encoding;
         }
 
-        // need this for mail(), PEAR::Mail not working
         $hdr = '';
         foreach($headers as $k => $v) {
             $hdr .= "$k: $v\n";
         }
         $hdr .= "\n";
+        return $hdr;
+    }
+
+    function send($recip, $subj, $msg) {
+
+        if (strtolower($this->encoding) == 'utf-8') {
+            $subj = "=?UTF-8?B?" . base64_encode($subj) . "?=";
+        }
 
         $params['sendmail_path'] = $this->sendmail_path;
 
         // Create the mail object using the Mail::factory method
         // $m =& Mail::factory('sendmail', $params); // it dont work right now
 
-        $res = mail($recip, $subj, $msg, $hdr);
+        $res = mail($recip, $subj, $msg, $this->get_headers());
         return $res;
 
     }
