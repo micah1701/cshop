e269ffd06f82959774acf181cf26d7b18c62650c
Sam Beam <sbeam@onsetcorps.net>
2011-08-04 15:01:10 -0400

    make cm_cart.purchased a regular int so it can track which order the cart turned into

--- util/alterations.sql
+++ util/alterations.sql
@@ -467,3 +467,5 @@ ALTER TABLE cm_giftcards ADD transaction_id VARCHAR(32);
 -- Wed Aug  3 16:07:47 EDT 2011
 ALTER TABLE cm_products MODIFY feature_rank tinyint(4) unsigned;
 
+-- Thu Aug  4 12:28:28 EDT 2011
+ALTER TABLE cm_cart MODIFY purchased int(10) unsigned;
